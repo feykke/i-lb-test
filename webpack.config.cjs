cf56cb7134729fd6f622d36f62fdcbc72d5506e2
feykke <re.office.25@gmail.com>
2022-07-04 14:47:28 -0300

    👷 Adding build system

---
+++ webpack.config.cjs
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: '',
+    // Define qual será o arquivo principal
+    entry: './src/index.js',
+
+    // Define onde será criado o arquivo bundle
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    // Define qual será o tratamento para cada tipo de arquivo
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
+    ]
+}
