5967a1b4d2bb6c2684a0021cb2b4bc642709426d
feykke <re.office.25@gmail.com>
2022-07-20 17:33:23 -0300

    ♻️ Changing the export method from index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { OutlineIcons } from'./components/outline/index.js'
-import { SolidIcons } from './components/solid/index.js'
-import { TwoColorIcons } from './components/twoColors/index.js'
-
-export { SolidIcons, OutlineIcons, TwoColorIcons }
+export { SolidIcons } from './components/solid/index'
+export { OutlineIcons } from './components/outline/index'
+export { TwoColorIcons } from './components/twoColors/index'
